2b5e2e1ffd913f36995f02a62e082c1329717b82
laudienp <paul.laudien@etu.unice.fr>
2022-05-14 16:30:29 +0200

    change mob to kroctor

--- game/ennemies/ennemy.js
+++ game/ennemies/ennemy.js
@@ -15,7 +15,7 @@ class ennemy
         var newmat = new BABYLON.StandardMaterial("noLight", scene);
         newmat.disableLighting = true;
         newmat.emissiveColor = BABYLON.Color3.White();
-        newmat.diffuseTexture = new BABYLON.Texture("https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/6/68/Dip.png", scene);
+        newmat.diffuseTexture = new BABYLON.Texture("./media/kroctor.png", scene, false, true, BABYLON.Texture.NEAREST_SAMPLINGMODE);
         newmat.diffuseTexture.hasAlpha = true;
         this.gameobject.material = newmat;
 
